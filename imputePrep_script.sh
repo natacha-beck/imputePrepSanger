e8d6726b3696bcb8632f4d3341e88de2e21483c2
Marie <eau.forest@gmail.com>
2016-08-22 15:44:16 -0400

    little error...

--- imputePrep_script.sh
+++ imputePrep_script.sh
@@ -23,9 +23,9 @@ DATAFILE=$dataPATH$1
 
 #Unzip a few files
 wget "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.gz"
-mv "human_g1k_v37.fasta.gz" "ressources/HRC_refSites/"
+mv "human_g1k_v37.fasta.gz" "ressources/HRC_refSites/human_g1k_v37.fasta.gz"
 wget "ftp://ngs.sanger.ac.uk/production/hrc/HRC.r1/HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz" 
-mv "HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz" "ressources/HRC_refSites/"
+mv "HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz" "ressources/HRC_refSites/HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz"
 gunzip "ressources/HRC_refSites/human_g1k_v37.fasta.gz" "ressources/HRC_refSites/"
 gunzip $hrc_RaynerCheckPATH"HRC.r1-1.GRCh37.wgs.mac5.sites.tab" $hrc_RaynerCheckPATH
 

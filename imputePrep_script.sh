7bea7ed73da1c878b1be6ca244aed7356cc0eb85
Marie <eau.forest@gmail.com>
2016-08-25 15:02:13 -0400

    little bug

--- imputePrep_script.sh
+++ imputePrep_script.sh
@@ -31,6 +31,14 @@ STRANDFILE=$dataPATH$2
 DATASTEM=$1
 DATAFILE=$dataPATH$1
  
+#Unzip a few files
+wget "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.gz"
+mv "human_g1k_v37.fasta.gz" $hrc_RaynerCheckPATH"human_g1k_v37.fasta.gz"
+wget "ftp://ngs.sanger.ac.uk/production/hrc/HRC.r1/HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz" 
+mv "HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz" $hrc_RaynerCheckPATH"HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz"
+gunzip $hrc_RaynerCheckPATH"human_g1k_v37.fasta.gz" 
+gunzip $hrc_RaynerCheckPATH"HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz"
+ 
  
 # Create binary file
 $PLINK_EXEC --file $DATAFILE  --make-bed --out $intermedPATH$DATASTEM"_binary"
@@ -43,20 +51,13 @@ $RAYNER_EXEC $intermedPATH$DATASTEM"_binary" $STRANDFILE $intermedPATH$DATASTEM"
 # QC steps,
 $PLINK_EXEC --bfile $intermedPATH$DATASTEM"_afterAlignment" --mind 0.1 --geno 0.1 --maf 0.05 --hwe 5e-8 --make-bed --out $intermedPATH$DATASTEM"_afterQC"
  
-#Unzip a few files
-wget "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.gz"
-mv "human_g1k_v37.fasta.gz" $hrc_RaynerCheckPATH"human_g1k_v37.fasta.gz"
-wget "ftp://ngs.sanger.ac.uk/production/hrc/HRC.r1/HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz" 
-mv "HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz" $hrc_RaynerCheckPATH"HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz"
-gunzip $hrc_RaynerCheckPATH"human_g1k_v37.fasta.gz" 
-gunzip $hrc_RaynerCheckPATH"HRC.r1.GRCh37.autosomes.mac5.sites.tab.gz"
  
  
 # Need to perform QC before the next command.
 # Also need the .bim and (from the plink --freq command) .frq files.
 $PLINK_EXEC --bfile $intermedPATH$DATASTEM"_afterAlignment" --freq --out $intermedPATH$DATASTEM"_afterAlignment_freq"
  
-perl $hrc_RaynerCheckPATH"HRC-1000G-check-bim_modified.pl" -b $intermedPATH$DATASTEM"_afterAlignment.bim" -f $intermedPATH$DATASTEM"_afterAlignment_freq.frq" -r $hrc_RaynerCheckPATH"HRC.r1-1.GRCh37.wgs.mac5.sites.tab" -h  
+perl $hrc_RaynerCheckPATH"HRC-1000G-check-bim_modified.pl" -b $intermedPATH$DATASTEM"_afterAlignment.bim" -f $intermedPATH$DATASTEM"_afterAlignment_freq.frq" -r $hrc_RaynerCheckPATH"HRC.r1.GRCh37.autosomes.mac5.sites.tab" -h  
  
 ## Now let's create the vcf files
 chmod u+x Run-plink.sh

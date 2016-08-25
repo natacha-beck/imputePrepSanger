bf09adfa86dd5356656394af2217537cdbef52d5
Marie <eau.forest@gmail.com>
2016-08-25 12:51:19 -0400

    playing around

--- imputePrep_script.sh
+++ imputePrep_script.sh
@@ -10,7 +10,8 @@ echo $2
 
 # Paths
 dataPATH="../data/"
-refStrandPATH="../data/"
+refStrandPATH="ressources/strand/"
+intermedPATH="../results/"
 resultsPATH="../data/"
 plinkPATH="tools/plink/"
 bcftoolsPATH="tools/bcftools/"
@@ -28,19 +29,19 @@ STRANDFILE=$refStrandPATH$2
 #DATAFILE=$dataPATH"MAVAN_PsychChip"
 #@arr = split(/./, $1);
 DATASTEM=$1
-DATAFILE=$1
+DATAFILE=$dataPATH$1
  
  
 # Create binary file
-$PLINK_EXEC --file $resultsPATH$DATAFILE  --make-bed --out $resultsPATH$DATASTEM"_binary"
+$PLINK_EXEC --file $DATAFILE  --make-bed --out $intermedPATH$DATASTEM"_binary"
  
  
 # Update build and strand
-$RAYNER_EXEC $resultsPATH$DATASTEM"_binary" $STRANDFILE $resultsPATH$DATASTEM"_afterAlignment"
+$RAYNER_EXEC $intermedPATH$DATASTEM"_binary" $STRANDFILE $intermedPATHPATH$DATASTEM"_afterAlignment"
  
  
 # QC steps,
-$PLINK_EXEC --bfile $resultsPATH$DATASTEM"_afterAlignment" --mind 0.1 --geno 0.1 --maf 0.05 --hwe 5e-8 --make-bed --out $resultsPATH$DATASTEM"_afterQC"
+$PLINK_EXEC --bfile $intermedPATH$DATASTEM"_afterAlignment" --mind 0.1 --geno 0.1 --maf 0.05 --hwe 5e-8 --make-bed --out $intermedPATH$DATASTEM"_afterQC"
  
 #Unzip a few files
 wget "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.gz"
@@ -53,17 +54,17 @@ gunzip $hrc_RaynerCheckPATH"HRC.r1-1.GRCh37.wgs.mac5.sites.tab"
  
 # Need to perform QC before the next command.
 # Also need the .bim and (from the plink --freq command) .frq files.
-$PLINK_EXEC --bfile $resultsPATH$DATASTEM"_afterAlignment" --freq --out $resultsPATH$DATASTEM"_afterAlignment_freq"
+$PLINK_EXEC --bfile $intermedPATH$DATASTEM"_afterAlignment" --freq --out $intermedPATH$DATASTEM"_afterAlignment_freq"
  
-perl $hrc_RaynerCheckPATH"HRC-1000G-check-bim_modified.pl" -b $resultsPATH$DATASTEM"_afterAlignment.bim" -f $resultsPATH$DATASTEM"_afterAlignment_freq.frq" -r $hrc_RaynerCheckPATH"HRC.r1-1.GRCh37.wgs.mac5.sites.tab" -h  
+perl $hrc_RaynerCheckPATH"HRC-1000G-check-bim_modified.pl" -b $intermedPATH$DATASTEM"_afterAlignment.bim" -f $intermedPATH$DATASTEM"_afterAlignment_freq.frq" -r $hrc_RaynerCheckPATH"HRC.r1-1.GRCh37.wgs.mac5.sites.tab" -h  
  
 ## Now let's create the vcf files
 chmod u+x Run-plink.sh
 ./Run-plink.sh
  
-$BCFTOOLS_EXEC annotate -Oz --rename-chrs ressources/HRC_refSites/ucsc2ensembl.txt results/$DATASTEM"_afterAlignment-updated_vcf.vcf.gz" > results/$DATASTEM"_afterAlignment-updatedChr_vcf.vcf.gz"
+$BCFTOOLS_EXEC annotate -Oz --rename-chrs $hrc_RaynerCheckPATH"ucsc2ensembl.txt" $intermedPATH$DATASTEM"_afterAlignment-updated_vcf.vcf.gz" > $resultsPATH$DATASTEM"_afterAlignment-updatedChr_vcf.vcf.gz"
  
-$BCFTOOLS_EXEC norm --check-ref e -f ressources/HRC_refSites/human_g1k_v37.fasta results/$DATASTEM"_afterAlignment-updatedChr_vcf.vcf.gz" -o $DATASTEM"_checkRef"
+$BCFTOOLS_EXEC norm --check-ref e -f $hrc_RaynerCheckPATH"human_g1k_v37.fasta" $resultsPATH$DATASTEM"_afterAlignment-updatedChr_vcf.vcf.gz" -o $resultsPATH$DATASTEM"_checkRef"
  
-$BCFTOOLS_EXEC index results/$DATASTEM"_afterAlignment-updatedChr_vcf.vcf.gz"
+$BCFTOOLS_EXEC index $resultsPATH$DATASTEM"_afterAlignment-updatedChr_vcf.vcf.gz"
  
